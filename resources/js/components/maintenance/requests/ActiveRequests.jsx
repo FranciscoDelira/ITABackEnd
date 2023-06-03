5d5451d8ab6c4c823c0446c7df42517c873a9eb9
Daniel Pineda <danielpinedajrz@outlook.com>
2023-06-02 23:08:36 -0600

    ActiveRequest

--- resources/js/components/maintenance/requests/ActiveRequests.jsx
+++ resources/js/components/maintenance/requests/ActiveRequests.jsx
@@ -9,6 +9,9 @@ import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Table from 'react-bootstrap/Table';
+import { Link } from "react-router-dom";
+import { Button } from 'react-bootstrap';
+import ReactDOM from 'react-dom';
 
 const theme={
     ThStyle:{
@@ -119,7 +122,7 @@ const ActiveRequests = () => {
                 }}
             />
 
-            <Table responsive>
+<Table responsive>
                 <thead>
                     <tr>
                         <th style={theme.ThStyle}>ID</th>
@@ -136,29 +139,17 @@ const ActiveRequests = () => {
                 </thead>
                 <tbody>
                     {filteredActives.map((active) => (
-                        <tr key={active.id}>
-                            <td> {active.id} </td>
-                            <td> {active.requestDate} </td>
-                            <td> {active.name} </td>
-                            <td> {active.department} </td>
-                            <td> {active.requestDescription} </td>
-                            <td> <img src={`/ITABackEnd/storage/app/${active.evidence1}`} alt="signature" width={100} height={100} /> </td>
-                            <td> <img src={`/ITABackEnd/storage/app/${active.evidence2}`} alt="signature" width={100} height={100} /> </td>
-                            <td> <img src={`/ITABackEnd/storage/app/${active.evidence3}`} alt="signature" width={100} height={100} /> </td>
+                        <tr key={active.id} onClick={() => handleClick(active.id)}>
+                            <td style={theme.TdStyle}> {active.id} </td>
+                            <td style={theme.TdStyle}> {active.requestDate} </td>
+                            <td style={theme.TdStyle}> {active.name} </td>
+                            <td style={theme.TdStyle}> {active.department} </td>
+                            <td style={theme.TdStyle}> {active.requestDescription} </td>
+                            <td> <img src={`/ITABackEnd/storage/app/${active.evidence1}`} alt="evidence1" width={100} height={100} /> </td>
+                            <td> <img src={`/ITABackEnd/storage/app/${active.evidence2}`} alt="evidence2" width={100} height={100} /> </td>
+                            <td> <img src={`/ITABackEnd/storage/app/${active.evidence3}`} alt="evidence3" width={100} height={100} /> </td>
                             <td> <img src={`/ITABackEnd/public/${active.signature}`} alt="signature" width={100} height={100} /> </td>
-                            <td> {active.status} </td>
-                            <td>
-                                <Stack direction='horizontal'>
-                                    <Button style={{ backgroundColor: '#1B396A', color: 'white', fontFamily: 'Montserrat', margin: '10%', height: 40, width: 100 }} as={Link} to={`http://localhost/ITABackEnd/public/newOrder/${active.id}`} >
-                                        Orden
-                                    </Button>
-                                    <Button style={{ backgroundColor: 'white', color: '#1B396A', fontFamily: 'Montserrat', height: 40, width: 90 }}
-                                        onClick={() => deleteRelease(release.id)}
-                                    >
-                                        Eliminar
-                                    </Button>
-                                </Stack>
-                            </td>
+                            <td style={theme.TdStyle}> {active.status} </td>
                         </tr>
                     ))}
                 </tbody>
